37e09acc68fc1172c5281faa9f157e98a16a9cef
ACER <ACER@LAPTOP-KRRKTQP5>
2018-12-06 22:19:21 +0700

    fix

---
+++ Latihan/src/view/EmployeesTableModel.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+import model.Employees;
+
+/**
+ *
+ * @author user only
+ */
+public class EmployeesTableModel extends AbstractTableModel {
+
+    private ArrayList<Employees> employeesList;
+    private String[] columns;
+
+    public EmployeesTableModel() {
+    }
+
+    public EmployeesTableModel(ArrayList<Employees> employeesList) {
+        super();
+        this.employeesList = employeesList;
+        columns = new String[]{
+            "Employee_ID", "First Name", "Last name", "Email"
+        };
+    }
+
+    public EmployeesTableModel(ArrayList<Employees> employeesList, String[] columns) {
+        super();
+        this.employeesList = employeesList;
+        this.columns = columns;
+    }
+
+    @Override
+    public int getRowCount() {
+        //To change body of generated methods, choose Tools | Templates.
+        return employeesList.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        //To change body of generated methods, choose Tools | Templates.
+        return columns.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        //To change body of generated methods, choose Tools | Templates.
+        return null;
+    }
+
+    // Optional, the name of your column
+    public String getColumnName(int col) {
+        return columns[col];
+    }
+
+}
